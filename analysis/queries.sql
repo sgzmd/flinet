fa0f43be122ca413fc3e497e19da66f29a4142bc
Roman Kirillov <sigizmund@gmail.com>
2023-12-05 10:17:24 +0000

    Fixing training with 2 epochs only

--- analysis/queries.sql
+++ analysis/queries.sql
@@ -49,3 +49,32 @@ WHERE
     'Эра Огня 5. Мятежное пламя' in natural language mode
   )
   and match(FirstName, LastName, MiddleName, NickName) AGAINST('Василий Криптонов' in natural language mode);
+
+
+
+
+DROP VIEW IF EXISTS TopRatedDetectedBooks;
+
+CREATE VIEW `TopRatedDetectedBooks` AS
+SELECT
+  `b`.`BookId` AS `bookId`,
+  `lb`.`Title` AS `Title`,
+  REPLACE(b.Annotation, '\n', '') AS Annotation, -- Replace new lines in Annotation
+  b.Authors,
+  count(0) AS `NumRecs`
+FROM
+  (
+    (
+      `DetectedBooks` `b`
+      join `libbook` `lb`
+    )
+    join `librecs` `lr`
+  )
+WHERE
+  `lb`.`BookId` = `b`.`BookId`
+  AND `lr`.`bid` = `b`.`BookId`
+GROUP BY
+  `b`.`BookId`,
+  `lb`.`Title`
+ORDER BY
+  count(0) DESC;
